77461207a35e41426f95c41905492f93483e6312
MarceloFantin <marcelo_fantin@yahoo.com.br>
2026-01-28 22:53:47 -0300

    28-01-2026
    inicio

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-df yyyy
+#pip  install fastapi uvicorn sqlalchemy passlib[bcrypt] python-jose[cryptography] python-dotenv python-multipart
+#para executar no terminal usar o comando uvicorn main:app --reload
+
+from fastapi import FastAPI
+
+app = FastAPI()
